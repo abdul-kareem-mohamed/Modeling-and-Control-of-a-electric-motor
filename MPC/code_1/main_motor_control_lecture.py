00fe4b15646d950d433fda78cc3d27ae912ccbfc
Abdul Kareem Mohamed <118671819+abdul-kareem-mohamed@users.noreply.github.com>
2026-01-19 12:21:56 +0100

    Add files via upload

---
+++ MPC/code_1/main_motor_control_lecture.py
@@ -0,0 +1,151 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import support_files_motor_lecture as sfm
+
+# Create an object for the support functions
+support=sfm.SupportFilesMotor()
+constants=support.constants
+
+# Load the constant values needed in the main file
+km=support.constants['motor_constant']
+R=support.constants['R']
+L=support.constants['L']
+r=support.constants['r'] # radius of the wheel
+J=support.constants['J']
+Ts=support.constants['Ts']
+time_length=support.constants['time_length'] # duration of the acceleration
+hz=support.constants['hz'] # horizon period
+Nu=support.constants['Nu'] # control horizon
+act_penalty=support.constants['R_weight']
+
+# Generate the reference signals
+t=np.arange(0,time_length+Ts,Ts) # time from 0 to 10 seconds, sample time (Ts=1 second)
+reference=20 #[rad/s]
+outputs=1
+sim_length=len(t) # Number of control loop iterations
+refSignals=np.zeros(sim_length*outputs)
+
+#Build up the reference signal vector:
+# refSignal = [20/r, 20/r, 20/r, .... etc.]
+for i in range(0,len(refSignals),outputs):
+    refSignals[i]=reference
+
+
+# Load the initial states
+w=0 # angular velocity
+I=0 # current
+
+states=np.array([w,I], dtype=np.float64)
+statesTotal=np.zeros((len(t),len(states))) # It will keep track of all your states during the entire manoeuvre
+statesTotal[0][0:len(states)]=states
+v_opt_total=np.zeros((len(t),hz))
+
+# Load the initial input
+Vin=0 # Initial Voltage input
+UTotal=np.zeros(len(t)) # To keep track all your inputs over time
+UTotal[0]=Vin
+
+# To extract w_opt from predicted x_aug_opt
+C_w_opt=np.zeros((hz,(len(states)+np.size(Vin))*hz))
+n_vars = 3 # Number of variables per time step
+for i in range(hz):
+    # i is the row (the time step)
+    # i * n_vars is the starting index of that time step's block
+    C_w_opt[i][i * n_vars] = 1
+
+# Generate the discrete state space matrices
+Ad,Bd,Cd,Dd=support.state_space()
+
+
+# Generate the compact simplification matrices for the cost function
+# The matrices (H, F) stay mostly constant during the simulation.
+# Therefore, it is more efficient to generate them here before you start the simulation loop.
+# However, in the end of the simulation, the horizon period (hz) will start decreasing.
+# That is when the matrices need to be regenerated (done inside the simulation loop)
+H,F=support.mpc_simplification(Ad,Bd,Cd,Dd,hz)
+m=1 #no. of inputs
+H_tilde = H[:, :Nu*m]
+# Initiate the controller - simulation loops
+k=0
+for i in range(0,sim_length-1):
+
+    # Generate the augmented current state and the reference vector
+    x_aug_t=np.transpose([np.concatenate((states,[Vin]),axis=0)])
+
+    # From the refSignals vector, only extract the reference values from your [current sample (NOW) + Ts] to [NOW+horizon period (hz)]
+    # Example: t_now is 3 seconds, hz = 15 samples, so from refSignals vectors, you move the elements to vector r:
+    # r=[w_ref_3.1, w_ref_3.2, ... , w_ref_4.5]
+    # With each loop, it all shifts by 0.1 second because Ts=0.1 s
+    k=k+outputs
+    if k+outputs*hz<=len(refSignals):
+        r=refSignals[k:k+outputs*hz]
+    else:
+        r=refSignals[k:len(refSignals)]
+        hz=hz-1
+
+    if hz<constants['hz']: # Check if hz starts decreasing
+        # These matrices (H,F) were created earlier at the beginning of the loop.
+        # They constant almost throughout the entire simulation. However,
+        # in the end of the simulation, the horizon period (hz) starts decreasing.
+        # Therefore, the matrices need to be constantly updated in the end of the simulation.
+        H,F=support.mpc_simplification(Ad,Bd,Cd,Dd,hz)
+        m=1 #no. of inputs
+        H_tilde = H[:, :Nu*m]
+
+    H_tilde_t = np.transpose(H_tilde)
+    HH=np.matmul(H_tilde_t, H_tilde)
+    lamdaI=act_penalty*np.eye(np.size(HH, 0))
+    Adb=HH + lamdaI
+    Adb_inv=np.linalg.inv(Adb)
+
+    r_t=np.transpose([r])
+    Fx=np.matmul(F, x_aug_t)
+    error=r_t-Fx
+    Herror=np.matmul(H_tilde_t, error)
+
+    du=np.matmul(Adb_inv, Herror)
+    Vin=Vin+du[0][0]  # Update the real inputs
+    UTotal[i+1]=Vin # Keep track of your inputs as you go from t=0 --> t=10 seconds
+
+    y_aug_opt=np.matmul(F,x_aug_t)+np.matmul(H_tilde,du)
+    v_opt_total[i+1][0:hz] = np.transpose(y_aug_opt)
+
+    # Compute new states in the open loop system (interval: Ts/30)
+    states=support.open_loop_new_states(states,Vin)
+    statesTotal[i+1][0:len(states)]=states
+
+# --- Plotting ---
+plt.figure(figsize=(10, 5))
+
+# Plot Reference vs Actual speed
+plt.plot(t, refSignals, 'r--', linewidth=2, label='Reference Speed')
+plt.plot(t, v_opt_total[:, 0], 'b-', linewidth=2, label='Actual Speed (Controlled)')
+
+plt.ylabel('Velocity (m/s)')
+plt.xlabel('Time (s)')
+plt.title('Closed-Loop Speed Control (Frictionless Motor)')
+plt.legend()
+plt.grid(True)
+
+plt.tight_layout()
+plt.show()
+
+# Plot the the input Vin
+plt.plot(t, UTotal[:], 'b-', linewidth=2, label='Input Voltage')
+
+plt.ylabel('Vin (V)')
+plt.xlabel('Time (s)')
+plt.title('Closed-Loop Speed Control (Frictionless Motor)')
+plt.legend()
+plt.grid(True)
+
+plt.tight_layout()
+plt.show()
+
+
+
+
+
+
+
+    
---
+++ MPC/code_1/support_files_motor_lecture.py
@@ -0,0 +1,176 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.linalg import expm
+from numpy.linalg import matrix_power
+
+class SupportFilesMotor:
+    ''' The following functions interact with the main file '''
+
+    def __init__(self):
+        '''Load the constants that do not change'''
+
+        # constants
+        motor_constant=1.25 #[N*m/A]
+        R=0.5 #[ohm]
+        L=0.01 #[H]
+        r=0.25 #[m]
+        m=80 #[kg]
+        J=m*r**2 #[kg*m**2]
+        Ts=0.01 # sampling time
+        time_length = 10 # [s] - duration of the entire manoeuvre
+        Nu=2 #control horizon
+
+        Q=np.matrix('1') # weights for outputs (all samples, except the last one) -  1 outputs, so 1x1 identity
+        S=np.matrix('10') # weights for the final horizon period outputs
+        R_weight=0.1 # weights for inputs (only 1 input in our case)
+
+        hz=10 #horizon period
+
+        self.constants={'motor_constant':motor_constant, 'R':R, 'L':L, 'r':r, 'J':J,\
+                        'Ts':Ts, 'time_length':time_length, 'hz':hz, 'Q':Q, 'S':S, 'R_weight':R_weight,\
+                            'Nu':Nu}
+
+        return None
+    
+
+    def discretize_zoh(self, A, B, T):
+        """
+        Discretize a continuous-time system (A, B) using Zero-Order Hold.
+        
+        Parameters:
+        A, B : ndarray (Continuous-time matrices)
+        T    : float (Sampling period)
+        """
+        n = A.shape[0]
+        m = B.shape[1]
+        
+        # 1. Construct the augmented matrix M
+        # [ A  B ]
+        # [ 0  0 ]
+        upper_row = np.hstack([A, B])
+        lower_row = np.zeros((m, n + m))
+        M = np.vstack([upper_row, lower_row])
+        
+        # 2. Compute the matrix exponential of (M * T)
+        Phi = expm(M * T)
+        
+        # 3. Extract Ad and Bd from the resulting matrix
+        Ad = Phi[:n, :n]
+        Bd = Phi[:n, n:]
+        
+        return Ad, Bd
+        
+    def state_space(self):
+        '''This function forms the state space matrices and transforms them in the discrete form'''
+
+        # Get the necessary constants
+        km=self.constants['motor_constant']
+        R=self.constants['R']
+        L=self.constants['L']
+        r=self.constants['r']
+        J=self.constants['J']
+        Ts = self.constants['Ts']
+
+        # Get the state space matrices for the control
+        A=np.array([[0, km/J], [-km/L, -R/L]])
+        B=np.array([[0], [1/L]])
+        Ct=np.array([[r, 0]])
+        D=0
+
+        # Discretise the system - zero-order hold method
+        Ad, Bd = self.discretize_zoh(A, B, Ts)
+        Cdt=Ct
+        Dd=D
+
+        return Ad, Bd, Cdt, Dd
+    
+    def mpc_simplification(self, Ad, Bd, Cdt, Dd, hz):
+        '''This function creates the compact matrices for Model Predictive Control'''
+        # db - double bar
+        # dbt - double bar transpose
+        # dc - double circumflex
+
+        A_aug=np.concatenate((Ad,Bd),axis=1)
+        temp1=np.zeros((np.size(Bd,1),np.size(Ad,1)))
+        temp2=np.identity(np.size(Bd,1))
+        temp=np.concatenate((temp1,temp2),axis=1)
+
+        A_aug=np.concatenate((A_aug,temp),axis=0)
+        B_aug=np.concatenate((Bd,np.identity(np.size(Bd,1))),axis=0)
+        C_aug=np.concatenate((Cdt,np.zeros((np.size(Cdt,0),np.size(Bd,1)))),axis=1)
+        D_aug=Dd
+
+        # --- Calculate F Matrix ---
+        # F = [ C_tilde * A_tilde ]
+        #     [ C_tilde * A_tilde^2 ]
+        #     ...
+        F_list = []
+        for k in range(1, hz + 1):
+            F_row = C_aug @ matrix_power(A_aug, k)
+            F_list.append(F_row)
+
+        F = np.vstack(F_list)
+
+        # --- Calculate H Matrix ---
+        # H is a Lower Triangular Toeplitz Matrix
+        # First column term: C_tilde * A_tilde^k * B_tilde (where power starts at 0 for diagonal)
+        H = np.zeros((hz, hz))
+
+        for i in range(hz):       # Rows (Future Time Steps)
+            for j in range(hz):   # Cols (Future Inputs)
+                if j <= i:
+                    power = i - j
+                    
+                    # If power is 0, A^0 = I, so term is C_tilde * B_tilde
+                    # If power > 0, term is C_tilde * A^power * B_tilde
+                    if power == 0:
+                        term = C_aug @ B_aug
+                    else:
+                        term = C_aug @ matrix_power(A_aug, power) @ B_aug
+                    
+                    H[i, j] = term.item() # .item() converts 1x1 array to scalar
+
+        return H,F
+    
+    def open_loop_new_states(self,states,U1):
+        '''This function computes the new state vector for one sample time later'''
+
+        current_states=states
+        new_states=current_states
+        w=current_states[0]
+        I=current_states[1]
+
+        km=self.constants['motor_constant']
+        R=self.constants['R']
+        L=self.constants['L']
+        r=self.constants['r']
+        J=self.constants['J']
+        Ts = self.constants['Ts']
+        
+        i_dot=(U1/L)-(km*w/L)-(R*I/L)
+        w_dot=km*I/J
+
+        w=w+w_dot*Ts
+        I=I+i_dot*Ts
+
+        new_states[0]=w
+        new_states[1]=I
+
+        return new_states
+
+
+# support=SupportFilesMotor()
+# Ad, Bd, Cdt, Dd = support.state_space()
+# H, F = support.mpc_simplification(Ad, Bd, Cdt, Dd, 3)
+# print("H: ")
+# print(H)
+# print("\n")
+# print("F: ")
+# print(F)
+# print("\n")
+# print("Cdt: ")
+# print(Cdt)
+# print("\n")
+# print("Dd: ")
+# print(Dd)
+# print("\n")
